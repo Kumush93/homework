5270aa54ff78eb2fc8685defa31103928dfe2b35
Mushtariy  Abdurashidova <mushtariyotaxonova26@gmail.com>
2024-01-29 00:21:07 +0500

    Bus ticket

---
+++ ticket.py
@@ -0,0 +1,27 @@
+def numbers_to_strings(argument):
+    match argument:
+        case 1:
+            return "VIP 210K"
+        case 2:
+            return "Econom 105 K"
+        case 3: 
+            return "Econom 105 K"
+        case 4: 
+            return "Econom 105 K"
+        case 5:
+            return "Econom 105 K"
+        case 6: 
+            return "Econom 105 K"
+        case 7: 
+            return "Econom 105 K"
+        case 8: 
+            return "Biznes 140 K"
+        case 9: 
+            return "Biznes 140 K",
+        case 10:
+            return "VIP 210 K"
+    
+    
+
+head=numbers_to_strings(int(input("Enter the number:")))
+print (head)
